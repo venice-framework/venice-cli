89749459a7976112d9e2a9a5ba43a70bc2cb860b
DavidPerich <davidnperich@gmail.com>
2020-04-03 18:34:40 +1100

    moving code to individual js files

--- src/cli.js
+++ src/cli.js
@@ -1,45 +1,47 @@
-const parseArgs = require("minimist");
 const fetch = require("node-fetch");
 
+const parseArgs = require("minimist");
+const clear = require("clear");
+const inquirer = require("inquirer");
+const connectorParse = require("./connectors");
+const getTopics = require("./topics");
+
 const log = arg => console.log(arg);
+
+//  URLS
+
+const SCHEMA_URL = "http://schema-registry:8081/subjects";
+const KAFKA_URL = "http://broker-1:9092";
+const KSQL_API_URL = "http://localhost:8088/ksql";
 
 export function cli(rawArgs) {
   const args = parseArgs(rawArgs);
   log(args);
   if (args.connectors) {
-    connectorCommands(args);
+    connectorParse(args);
+  } else if (args.schemas) {
+    schemaParse(args);
+  } else if (args.topics) {
+    getTopics();
   }
 }
 
-// connectors
-const CONNECTORS_URL = "http://localhost:8083/connectors";
-const connectorCommands = args => {
+// TOPICS
+
+// SCHEMAS
+
+const schemaLogic = args => {
   if (args.new) {
     newConnector();
   } else {
-    showConnectors();
+    showSchemas();
   }
 };
 
-const showConnectors = () => {
-  fetch(CONNECTORS_URL)
+const showSchemas = () => {
+  fetch(SCHEMA_URL)
     .then(res => res.json())
-    .then(json => log(json));
+    .then(json => log(json))
+    .catch(err => log(err));
   // TODO - LOOK to do the prettier version saved in lib/show-connectors.js
-};
-
-const newConnector = () => {
-  // TODO - extract json to lib/insert-connector
-  // TODO - think of someway to have have insert OR upsert
-
-  let info = require("../lib/insert-connector.json");
-  log(info);
-
-  fetch(CONNECTORS_URL, {
-    method: "POST",
-    body: JSON.stringify(info),
-    headers: { "Content-Type": "application/json" }
-  })
-    .then(res => res.json())
-    .then(json => log(json));
 };
