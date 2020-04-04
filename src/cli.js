776e529413c2f92396f571f7dac304de9da325e6
Melissa Manousos <mmanousos@gmail.com>
2020-04-03 20:16:54 -0700

    Add commands for docker also marked out is approach for aliases

--- src/cli.js
+++ src/cli.js
@@ -15,11 +15,54 @@ const log = arg => console.log(arg);
 const SCHEMA_URL = "http://schema-registry:8081/subjects";
 const KSQL_API_URL = "http://localhost:8088/ksql";
 
-// this is the logic for the CLI.
+// outlines the commands and their aliases
+// ** just playing with this idea - not sure if it's a good one ** 
+// const argumentsIntoOptions = rawArgs => {
+//   const args = parseArgs(
+//     {
+//       "--connector": Boolean,
+//       "--down": Boolean,
+//       "--elastic": Boolean,
+//       "--ksql": Boolean,
+//       "--log": Boolean,
+//       "--schema": Boolean,
+//       "--status": Boolean,
+//       "--restart": Boolean,
+//       "--topic": Boolean,
+//       "--psql": Boolean,
+//       "--up": Boolean,
+//       "-c": "--connector",
+//       "-s": "--schema",
+//       "-t": "--topic",
+//       "-r": "--restart"
+//     },
+//     {
+//       argv: rawArgs.slice(2)
+//     }
+//   );
+//   console.log(args);
+//   console.log(argv);
 
+//   return {
+//     connector: args["--connector"] || false,
+//     down: args["--down"] || false,
+//     elastic: args["--elastic"] || false,
+//     ksql: args["--ksql"] || false,
+//     log: args["--log"] || false,
+//     psql: args["--psql"] || false,
+//     restart: args["--restart"] || false,
+//     schema: args["--schema"] || false,
+//     status: args["--status"] || false,
+//     topic: args["--topic"] || false,
+//     up: args["--up"] || false
+//   };
+// };
+
+// this is the logic for the CLI.
 export function cli(rawArgs) {
+  // const args = argumentsIntoOptions(rawArgs);
   const args = parseArgs(rawArgs);
-  log(args);
+  // log(args);
   if (args.connectors) {
     connectorParse(args);
   } else if (args.schemas) {
@@ -28,18 +71,15 @@ export function cli(rawArgs) {
     getTopics();
   }
 
-  switch (args._[2]) {
-    case "up":
-      docker.up();
-      break;
-    case "down":
-      docker.down();
-      break;
-    case "status":
-      docker.status();
-      break;
-    case "log":
-      docker.log(args._[3]); // this may not be the best way to accomplish this - they may want to see logs for more than 1 container
-      break;
+  if (args.down) {
+    docker.down();
+  } else if (args.log) {
+    docker.log();
+  } else if (args.restart) {
+    docker.restart();
+  } else if (args.status) {
+    docker.status();
+  } else if (args.up) {
+    docker.up();
   }
 }
