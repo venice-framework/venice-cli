8e6dfc9a789fad51382ed3bd7b64a65ee5492c85
DavidPerich <davidnperich@gmail.com>
2020-04-06 09:26:36 +1000

    removing todo's from connectors and adding error for no topics

--- src/connectors.js
+++ src/connectors.js
@@ -10,14 +10,8 @@ const CONNECT_URL = "http://localhost:8083/connectors";
 const CONNECT = {
   // DOCS -    // error parsing  - https://docs.confluent.io/current/connect/references/restapi.html
   // DOCS for woerk config - https://docs.confluent.io/current/connect/references/allconfigs.html
-  // Confirm we have kafka-connect in distributed mode
+  // TODO - Confirm we have kafka-connect in distributed mode
   // TODO - change connector so its not hard coded to buses. Maybe we just have venice database.
-  // TODO success message if 201.
-  // TODO Connectors Status
-  // TODO connector plugings
-  // get installed plugins in the cluster
-  // error parsing  - https://docs.confluent.io/current/connect/references/restapi.html
-  // TODO success message if 201.
   parseConnectorCommand: args => {
     // TODO - make sure this switch statement works with all the aliases
     switch (args.c) {
@@ -100,36 +94,40 @@ const CONNECT = {
   },
 
   setQuestions: topics => {
-    // Add try/catch here for if topics doesn't exist (aka the service isn't reachable)
-    // TODO - Add error message explaining that unable to get topics.
-    return [
-      {
-        type: "list",
-        name: "sink",
-        message: "which data sink are you adding new connection?",
-        choices: ["Postgres", "Elastic Search"]
-      },
-      {
-        type: "input",
-        name: "connector_name",
-        message: "What would you like to name the new sink connection?"
-      },
-      {
-        type: "list",
-        name: "topic",
-        message: "which topic do you want to sink?",
-        choices: topics
-      },
-      {
-        type: "list",
-        name: "insert_mode",
-        message: "Do you want insert or upsert as insert mode?",
-        choices: [
-          "insert: Create new rows - if there is a primary key clash there will be a postgres error",
-          "upsert: Insert or update rows - if primary key exists, row will be updated. If not then a new row is created"
-        ]
-      }
-    ];
+    if (!topics) {
+      throw new Error(
+        "Unable to get topics, please make sure kafka brokers are running"
+      );
+    } else {
+      return [
+        {
+          type: "list",
+          name: "sink",
+          message: "which data sink are you adding new connection?",
+          choices: ["Postgres", "Elastic Search"]
+        },
+        {
+          type: "input",
+          name: "connector_name",
+          message: "What would you like to name the new sink connection?"
+        },
+        {
+          type: "list",
+          name: "topic",
+          message: "which topic do you want to sink?",
+          choices: topics
+        },
+        {
+          type: "list",
+          name: "insert_mode",
+          message: "Do you want insert or upsert as insert mode?",
+          choices: [
+            "insert: Create new rows - if there is a primary key clash there will be a postgres error",
+            "upsert: Insert or update rows - if primary key exists, row will be updated. If not then a new row is created"
+          ]
+        }
+      ];
+    }
   },
 
   promptUserInput: questions => {
@@ -137,11 +135,9 @@ const CONNECT = {
   },
 
   mergeAnswersWithTemplate: (answers, topics) => {
-    // TODO - Need to think about the number of tasks, its based on the number of topics and the number of partitions
     // TODO - make it an option to have multiple topics
     // TODO - need to think about key deserialisation.
     // TODO - how do we get the database name - currently hardcoded to buses
-    // TODO - need to think about how many tasks to spin up - should be equal to the number of partitions for the topic.
     // TODO - will this filepath from anywhere?
     let response;
 
@@ -183,7 +179,6 @@ const CONNECT = {
   },
 
   postNewConnectorRequest: async answers => {
-    // #TODO
     const resp = await fetch(CONNECT_URL, {
       method: "POST",
       body: JSON.stringify(answers),
