d53f93342735fe01f89291cbeb7dc1f670667fa2
Melissa Manousos <mmanousos@gmail.com>
2020-04-04 20:44:03 -0700

    Add notes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+// TODO: determine why cli is being read insted of this file.
 
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
 
 // clear the terminal after the command is issued
 clear();
 
 // display our project name in blue ASCII art
 console.log(
-  chalk.hex('#3282b8').dim(
-    figlet.textSync('VENICE', { horizontalLayout: 'full' })
-  )
+  chalk
+    .hex("#3282b8")
+    .dim(figlet.textSync("VENICE", { horizontalLayout: "full" }))
 );
-
-// // another option is to simply display the name, slightly brighter and underlined (no ASCII)
-// console.log(
-//   chalk.hex('#3282b8').underline('VENICE')
-// );
