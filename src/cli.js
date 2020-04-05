bdf74d7f779b3404c157448d0252613a024e8b84
Melissa Manousos <mmanousos@gmail.com>
2020-04-04 20:43:50 -0700

    Remove placeholders, add notes

--- src/cli.js
+++ src/cli.js
@@ -1,68 +1,22 @@
-const fetch = require("node-fetch");
-
+const fetch = require("node-fetch"); // can remove?
 const parseArgs = require("minimist");
-const clear = require("clear");
-const inquirer = require("inquirer");
+const clear = require("clear"); // can remove?
+const inquirer = require("inquirer"); // can remove?
 const connectorParse = require("./connectors");
 const getTopics = require("./topics");
-const getSchemas = require("./schemas");
+const getSchemas = require("./schemas"); // should this be schemaParse?
 const docker = require("./docker");
 
-const log = arg => console.log(arg);
+const log = arg => console.log(arg); // can remove?
 
 //  URLS - eventually these should all be docker URLS or ENV variables
 
 const SCHEMA_URL = "http://schema-registry:8081/subjects";
 const KSQL_API_URL = "http://localhost:8088/ksql";
 
-// outlines the commands and their aliases
-// ** just playing with this idea - not sure if it's a good one ** 
-// const argumentsIntoOptions = rawArgs => {
-//   const args = parseArgs(
-//     {
-//       "--connector": Boolean,
-//       "--down": Boolean,
-//       "--elastic": Boolean,
-//       "--ksql": Boolean,
-//       "--log": Boolean,
-//       "--schema": Boolean,
-//       "--status": Boolean,
-//       "--restart": Boolean,
-//       "--topic": Boolean,
-//       "--psql": Boolean,
-//       "--up": Boolean,
-//       "-c": "--connector",
-//       "-s": "--schema",
-//       "-t": "--topic",
-//       "-r": "--restart"
-//     },
-//     {
-//       argv: rawArgs.slice(2)
-//     }
-//   );
-//   console.log(args);
-//   console.log(argv);
-
-//   return {
-//     connector: args["--connector"] || false,
-//     down: args["--down"] || false,
-//     elastic: args["--elastic"] || false,
-//     ksql: args["--ksql"] || false,
-//     log: args["--log"] || false,
-//     psql: args["--psql"] || false,
-//     restart: args["--restart"] || false,
-//     schema: args["--schema"] || false,
-//     status: args["--status"] || false,
-//     topic: args["--topic"] || false,
-//     up: args["--up"] || false
-//   };
-// };
-
 // this is the logic for the CLI.
 export function cli(rawArgs) {
-  // const args = argumentsIntoOptions(rawArgs);
   const args = parseArgs(rawArgs);
-  // log(args);
   if (args.connectors) {
     connectorParse(args);
   } else if (args.schemas) {
