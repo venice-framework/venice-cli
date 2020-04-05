a5769d2217da2f58260af5f48ddb8139581deb89
Melissa Manousos <mmanousos@gmail.com>
2020-04-04 20:37:50 -0700

    Adds restart functionality

--- src/docker.js
+++ src/docker.js
@@ -16,27 +16,25 @@ const dockerInstance = new Docker();
 
 const docker = {
   // TODO add spinner or progress text e.g shutting down broker 1, shutting down zookeper
+  // TODO confirmclear this won't shut down all docker compose files!
   down: () => {
     exec("docker-compose down").on("close", () => {
       log("Venice has shut down.");
     });
   },
 
-  // also not working yet - same code as `log` - but doesn't return an error
   restart: async () => {
-    const services = await askWhichServices("restart");
-    log(services);
-    try {
-      start = await execa("docker", `restart ${services}`);
-    } catch (err) {
-      err(err);
+    const services = await multiple("restart");
+
+    const start = await dockerInstance.command(`restart ${services}`);
+    if (!start) {
+      error(`${services} could not be restarted`);
       return;
+    } else {
+      docker.status();
+      log(`${services} restarted successfully`);
     }
-    docker.status();
-    log(`${services} restarted successfully`);
   },
-  //restartMany(services, options) - Restart services
-  // restartOne(service, options) - Restart service
 
   status: () => {
     exec("docker ps", (err, stdout, stderr) => {
@@ -44,39 +42,24 @@ const docker = {
     });
   },
 
+  // TODO: add an async await to make sure this actually runs & return error if there is a conflict (another container with same name, etc.)
   up: () => {
     exec("docker-compose up -d --build").on("close", () => {
       log("Venice is now running.");
     });
   },
 
-  // this command asks which of the Venice services they want to see logs for
-  // they'll have to use the standard `docker logs -f` command for their own containers
-  // ** WIP: not currently accepting the command **
+  // TODO: not formatting error correctly - refactor to use promises error handling
   log: async () => {
     // determine which service they want to log - can only log 1 at a time
-    const services = await single("log");
+    const service = await single("log");
 
-    // pipe logs to the console
-    try {
-      dockerInstance.command(`logs -f ${services}`);
-    } catch (err) {
-      error(`There's no running container called ${services}`);
+    const logs = await dockerInstance.command(`logs -f ${service}`);
+    if (!logs) {
+      error(`${service} isn't present`);
+      return;
     }
   }
 };
-
-// How Chronos handles logging, but I tried this approach and it also didn't work
-// const logs = service => {
-//   if (SERVICES.includes(service)) {
-//     exec(`docker-compose logs ${service}`, (err, stdout, stderr) =>
-//       log(stdout)
-//     );
-//   } else {
-//     log(`${service} is not a valid service.`);
-//     log(`format: chronos logs (service)
-//       ${SERVICE_LISTING}`);
-//   }
-// };
 
 module.exports = docker;
