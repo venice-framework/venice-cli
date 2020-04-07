0bd736aec0d998ed7db08df74c0f40be648cbc0c
Melissa Manousos <mmanousos@gmail.com>
2020-04-07 14:57:25 -0700

    Remove unused components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-// TODO: determine why cli is being read insted of this file.
+// // TODO: determine why cli is being read insted of this file.
 
-const chalk = require("chalk");
-const clear = require("clear");
-const figlet = require("figlet");
+// const chalk = require("chalk");
+// const clear = require("clear");
+// const figlet = require("figlet");
 
-// clear the terminal after the command is issued
-clear();
-
-// display our project name in blue ASCII art
-console.log(
-  chalk
-    .hex("#3282b8")
-    .dim(figlet.textSync("VENICE", { horizontalLayout: "full" }))
-);
+// // clear the terminal after the command is issued
+// clear();
