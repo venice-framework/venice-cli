0847bdb92e9d82545d5e6b647f18304b221ff6f9
DavidPerich <davidnperich@gmail.com>
2020-04-07 17:19:48 +1000

    Added print schemas on schema-registry

--- src/schemas.js
+++ src/schemas.js
@@ -1,17 +1,57 @@
-// const schemaLogic = args => {
-//   if (args.new) {
-//     newConnector();
-//   } else {
-//     showSchemas();
-//   }
-// };
+const { log, error, fetch, divider } = require("../utils");
+// const SCHEMA_URL = "http://schema-registry:8081/subjects";
+const SCHEMA_URL = "http://localhost:8081/subjects";
+
+const validSchemaCommands = `
+  "venice schemas": prints all the current schemas
+
+  "venice -s" is an alias for "venice schemas"
+`;
+
+const SCHEMAS = {
+  // TODO - GET the actual SCHEMA for each schema
+  parseSchemaCommand: command => {
+    switch (command) {
+      case false:
+        SCHEMAS.printSchemas();
+        break;
+
+      default:
+        error(
+          `"${command}" is not a valid schema command. The list of valid commands are:
+
+          ${validSchemaCommands}`
+        );
+        break;
+    }
+  },
+
+  getSchemas: async () => {
+    return await fetch(SCHEMA_URL)
+      .then(res => res.json())
+      .catch(err => log(err));
+  },
+
+  printSchemas: async () => {
+    const schemas = await SCHEMAS.getSchemas();
+    console.log(schemas);
+
+    if (schemas.length === 0) {
+      log("There are no schemas currently ");
+      return;
+    }
+
+    log(`There are ${schemas.length} schemas:`);
+    divider();
+    schemas.forEach(log);
+  }
+};
 
 // I haven't tested this as the schema-registry isn't available on local host
 const getSchemas = () => {
-  fetch(SCHEMA_URL)
+  return fetch(SCHEMA_URL)
     .then(res => res.json())
-    .then(json => log(json))
     .catch(err => log(err));
 };
 
-module.exports = getSchemas;
+module.exports = SCHEMAS;
