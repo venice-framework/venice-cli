fffb353e3cac1697df8436bd0f2d53928d698ca0
Melissa Manousos <mmanousos@gmail.com>
2020-04-08 15:47:19 -0700

    Reorder and revise manual descriptions

--- src/manual.js
+++ src/manual.js
@@ -2,29 +2,35 @@ const { blue, clui, divider2, log } = require("../utils");
 const Line = clui.Line;
 
 const commands = [
-  ["install", "-i", "select a venice-pipeline to download from GitHub"],
-
-  ["up", "", "launch venice pipeline"],
-
-  ["down", "", "shut down venice pipeline"],
-  ["status", "-st", "status of venice components"],
-  // ["elasticsearch", "-es", ""][("ksql", "-k", "launch ksql CLI")], // uncomment if we get this working - otherwise remove this
-  ["logs", "-l", "view logs of venice components"],
-  ["restart", "-r", "restart venice components"],
-  ["psql", "-p", "launch the postgreSQL CLI"],
+  ["down", "", "shut down the venice pipeline"],
+  // ["elasticsearch", "-es", ""] // uncomment if we get this working - otherwise remove this
+  ["install", "-i", "download and install a venice component from GitHub"],
   ["ksql", "-k", "launch the ksqlDB CLI"],
-  ["schemas", "-s", "view current schemas saved in the Schema Registry"]
+  ["logs", "-l", "view logs of venice components"],
+  ["psql", "-p", "launch the postgreSQL CLI"],
+  ["restart", "-r", "restart venice components"],
+  ["schemas", "-s", "view schemas saved in the Schema Registry"],
+  ["status", "-st", "view the status of venice components"],
+  ["up", "", "launch the venice pipeline"],
 ];
 
 const connectorCommands = [
-  ["connectors", "-c", "view current connectors and their status"],
-  ["connectors new", "-c new", "create a new connection to existing data sink"],
-  ["connectors delete", "-c delete", "delete an existing connection"]
+  ["connectors", "-c", "view existing connectors and their status"],
+  [
+    "connectors new",
+    "-c new",
+    "create a new connection to an existing data sink",
+  ],
+  ["connectors delete", "-c delete", "delete an existing connection"],
 ];
 
 const topicCommands = [
   ["topics", "-t", "view a list of the current topics"],
-  ["topics show", "-t show", "stream events from an existing topic to terminal"]
+  [
+    "topics show",
+    "-t show",
+    "stream events from an existing topic to terminal",
+  ],
 ];
 
 const manual = {
@@ -35,19 +41,19 @@ const manual = {
     let headers = new Line()
       .padding(2)
       .column("Command", 20, [blue])
-      .column("Alias", 13, [blue])
-      .column("Function", 50, [blue])
+      .column("Alias", 12, [blue])
+      .column("Function", 55, [blue])
       .fill()
       .output();
 
     divider2();
 
-    commands.forEach(command => {
+    commands.forEach((command) => {
       new Line()
         .padding(2)
         .column(command[0], 20, [blue])
-        .column(command[1], 13, [blue])
-        .column(command[2], 50, [blue])
+        .column(command[1], 12, [blue])
+        .column(command[2], 55, [blue])
         .fill()
         .output();
     });
@@ -56,12 +62,12 @@ const manual = {
     log("Connect Commands");
     divider2();
 
-    connectorCommands.forEach(command => {
+    connectorCommands.forEach((command) => {
       new Line()
         .padding(2)
         .column(command[0], 20, [blue])
-        .column(command[1], 13, [blue])
-        .column(command[2], 50, [blue])
+        .column(command[1], 12, [blue])
+        .column(command[2], 55, [blue])
         .fill()
         .output();
     });
@@ -69,7 +75,7 @@ const manual = {
     divider2();
     log("Topic Commands");
     divider2();
-    topicCommands.forEach(command => {
+    topicCommands.forEach((command) => {
       new Line()
         .padding(2)
         .column(command[0], 20, [blue])
@@ -82,7 +88,7 @@ const manual = {
     log(
       "\nTo view this guide at any time, use 'venice man' or 'venice --help'.\n"
     );
-  }
+  },
 };
 
 module.exports = manual;
